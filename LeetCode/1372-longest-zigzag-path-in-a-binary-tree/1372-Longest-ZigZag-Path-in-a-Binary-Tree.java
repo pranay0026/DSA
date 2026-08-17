27ebfb75453e16d5662ee495438587e298978d92
pranay0026 <pranaykoppineedi@gmail.com>
2026-08-17 20:52:50 +0530

    [1372] [Time Beats: 62.15%] [Memory Beats: 55.34%] - LeetPush

---
+++ LeetCode/1372-longest-zigzag-path-in-a-binary-tree/1372-Longest-ZigZag-Path-in-a-Binary-Tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxLen=0;
+    public int longestZigZag(TreeNode root) {
+        if(root==null) return 0;
+        dfs(root.left,true,1);
+        dfs(root.right,false,1);
+        return maxLen;
+    }
+    private void dfs(TreeNode node, boolean isLeft,int length){
+        if(node==null) return;
+        maxLen=Math.max(maxLen,length);
+        if(isLeft){
+            dfs(node.right,false,length+1);
+            dfs(node.left,true,1);
+        }
+        else{
+            dfs(node.left,true,length+1);
+            dfs(node.right,false,1);
+        }
+    }
+}
