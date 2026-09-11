60487649eebc53b00010a94eec1e1dd1d41ad1aa
pranay0026 <pranaykoppineedi@gmail.com>
2026-09-11 22:32:16 +0530

    [925] [Time Beats: -%] [Memory Beats: -%] - LeetPush

---
+++ LeetCode/925-long-pressed-name/925-Long-Pressed-Name.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isLongPressedName(String name,String typed) {
+        int p1=0,p2=0;
+        while(p2<typed.length()){
+            if(p1<name.length()&&name.charAt(p1)==typed.charAt(p2)){
+                p1++;
+                p2++;
+            }
+            else if(p2>0&&typed.charAt(p2)==typed.charAt(p2-1)){
+                p2++;
+            }
+            else{
+                return false;
+            }
+        }
+        return p1==name.length();
+    }
+}
