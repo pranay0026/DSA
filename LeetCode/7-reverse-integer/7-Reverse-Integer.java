66e64c2eb7f46e5b175b3a6d47d8c11e30845b37
pranay0026 <pranaykoppineedi@gmail.com>
2026-09-26 20:04:50 +0530

    [7] [Time Beats: 99.96%] [Memory Beats: 26.91%] - LeetPush

---
+++ LeetCode/7-reverse-integer/7-Reverse-Integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        int n=x;
+        int sign=1;
+        if(n<0){
+            n=-n;
+            sign=-1;
+        }
+        int rev=0;
+        while(n>0){
+            int digit=n%10;
+            if(rev>Integer.MAX_VALUE/10)
+                return 0;
+            if(rev==Integer.MAX_VALUE/10 && digit>7)
+                return 0;
+            rev=rev*10+digit;
+            n/=10;
+        }
+        return rev*sign;
+    }
+}
